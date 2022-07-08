14f3bd6eccb565618f8c15b79fcd150012dcc375
Andrew Ellis <andrewellis@pm.me>
2022-07-08 11:50:19 +0200

    Update Psychophysics.jl

--- src/Psychophysics.jl
+++ src/Psychophysics.jl
@@ -1,5 +1,11 @@
 module Psychophysics
 
-# Write your package code here.
+using Base: @kwdef
+using Random, Distributions
+
+export StaircaseMethod, PestMethod, QuestMethod
+export PsiMethod, MDPMethod
+
+include("adaptive-methods.jl")
 
 end
