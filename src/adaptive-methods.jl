32901b01fb5015bdb6f0e830d302554644539c53
Andrew Ellis <andrewellis@pm.me>
2022-07-08 11:50:15 +0200

    Create adaptive-methods.jl

---
+++ src/adaptive-methods.jl
@@ -0,0 +1,21 @@
+abstract type AbstractMethod end
+
+@kwdef struct StaircaseMethod <: AbstractMethod
+    a::Real = 0
+end
+
+@kwdef struct PestMethod <: AbstractMethod
+    a::Real = 0
+end
+
+@kwdef struct QuestMethod <: AbstractMethod
+    a::Real = 0
+end
+
+@kwdef struct PsiMethod <: AbstractMethod
+    a::Real = 0
+end
+
+@kwdef struct MDPMethod <: AbstractMethod
+    a::Real = 0
+end
